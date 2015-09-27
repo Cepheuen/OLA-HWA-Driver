cbf0a552adfea64ce3b12302b2645b15f793e29a
Ashik Vetrivelu <vcashik@gmail.com>
2015-09-27 09:17:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/cepheuen/olahwadriver/app/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.cepheuen.olahwadriver.app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by ashikvetrivelu on 27/09/15.
+ * */
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+            }
+        }, 1800);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_base, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/cepheuen/olahwadriver/app/utils/APIKeys.java
@@ -0,0 +1,9 @@
+package com.cepheuen.olahwadriver.app.utils;
+
+/**
+ * Created by ashikvetrivelu on 27/09/15.
+ */
+public class APIKeys {
+    public static String ACCESS_CODE = "bec5cefda91c47f8b87b6f23aa60d6fd";
+    public static String X_APP_Token = "2c623e867c1a4cf39c43a4757a207f08";
+}
